147697cdcbe15e0cd4218610ad9949a1c40bdf71
Alexis Roda <alexis.roda.villalonga@gmail.com>
2017-08-25 21:09:43 +0200

    Cambiar el formato de la definición de los niveles.
    Para ahorrar memoria en la definición de los niveles se añade un
    elemento que permite especificar la repetición del tile, tanto
    horizontal como verticalmente.
    
    Se adapta la function de pintado del nivel.

--- src/levels.c
+++ src/levels.c
@@ -5,9 +5,11 @@
 
 #define BRICK 0
 #define SAND 1
-
 #define END 0xFF
 
+#define REP_HOR 0x80
+#define REP_VER 0x00
+#define REP_MASK 0x7F
 
 static const u8* const tile_set[] = {
     SpriteBrick,
@@ -15,88 +17,40 @@ static const u8* const tile_set[] = {
 };
 
 /*
- * Each level is a list of triples specifying the coordinates, in grid units,
- * of the element and the element. The list ends with a 0xFF byte.
+ * Cada nivel es una lista de cuartetos que especifican:
+ *
+ * - la coordenada x. En unidades de la cuadrícula.
+ * - la coordenada y. En unidades de la cuadrícula.
+ * - el número de repeticiones. El bit maś alto determina si la repetición es
+ *   horizontal (REP_HOR) o vertical (REP_VER). Esto permite un máximo de 127
+ *   repeticiones.
+ * - el tile a pintar.
+ *
+ * La definición del mapa debe acabar con un byte END.
+ *
  */
 
 static const u8 level_00[] = {
-     0, 0, BRICK,
-     1, 0, BRICK,
-     2, 0, BRICK,
-     3, 0, BRICK,
-     4, 0, BRICK,
-     5, 0, BRICK,
-     6, 0, BRICK,
-     7, 0, BRICK,
-     8, 0, BRICK,
-     9, 0, BRICK,
-     10, 0, BRICK,
-     11, 0, BRICK,
-     12, 0, BRICK,
-     13, 0, BRICK,
-     14, 0, BRICK,
-     15, 0, BRICK,
-     16, 0, BRICK,
-     17, 0, BRICK,
-     18, 0, BRICK,
-     19, 0, BRICK,
-
-     0, 1, BRICK, 19, 1, BRICK,
-     0, 2, BRICK, 19, 2, BRICK,
-     0, 3, BRICK, 19, 3, BRICK,
-     0, 4, BRICK, 19, 4, BRICK,
-     0, 5, BRICK, 19, 5, BRICK,
-     0, 6, BRICK, 19, 6, BRICK,
-     0, 7, BRICK, 19, 7, BRICK,
-     0, 8, BRICK, 19, 8, BRICK,
-     0, 9, BRICK, 19, 9, BRICK,
-     0, 10, BRICK, 19, 10, BRICK,
-     0, 11, BRICK, 19, 11, BRICK,
-     0, 12, BRICK, 19, 12, BRICK,
-     0, 13, BRICK, 19, 13, BRICK,
-     0, 14, BRICK, 19, 14, BRICK,
-     0, 15, BRICK, 19, 15, BRICK,
-     0, 16, BRICK, 19, 16, BRICK,
-     0, 17, BRICK, 19, 17, BRICK,
-
-     0, 18, BRICK,
-     1, 18, BRICK,
-     2, 18, BRICK,
-     3, 18, BRICK,
-     4, 18, BRICK,
-     5, 18, BRICK,
-     6, 18, BRICK,
-     7, 18, BRICK,
-     8, 18, BRICK,
-     9, 18, BRICK,
-     10, 18, BRICK,
-     11, 18, BRICK,
-     12, 18, BRICK,
-     13, 18, BRICK,
-     14, 18, BRICK,
-     15, 18, BRICK,
-     16, 18, BRICK,
-     17, 18, BRICK,
-     18, 18, BRICK,
-     19, 18, BRICK,
-
+    0,  0, REP_HOR | 20, BRICK,
+    0,  1, REP_VER | 17, BRICK,
+    19, 1, REP_VER | 17, BRICK,
+    0, 18, REP_HOR | 20, BRICK,
     END
 };
 
 static const u8 level_01[] = {
-     9, 9, BRICK,
-    10, 9, BRICK,
+    9, 9, REP_HOR | 2, BRICK,
     END
 };
 
 static const u8 level_02[] = {
-     9, 0, BRICK,
-     9, 3, BRICK,
-     9, 6, BRICK,
-     9, 9, BRICK,
-     9, 12, BRICK,
-     9, 15, BRICK,
-     9, 18, BRICK,
+     9,  0, REP_HOR | 2, BRICK,
+     9,  3, REP_HOR | 2, BRICK,
+     9,  6, REP_HOR | 2, BRICK,
+     9,  9, REP_HOR | 2, BRICK,
+     9, 12, REP_HOR | 2, BRICK,
+     9, 15, REP_HOR | 2, BRICK,
+     9, 18, REP_HOR | 2, BRICK,
     END
 };
 
@@ -109,17 +63,33 @@ const TLevel g_levels[NUM_LEVELS] = {
 void draw_level(const TLevel *level) {
     u8 const *map = level->map;
     u8 *ptr;
+    u8 rep;
     u8 x;
     u8 y;
+    u8 dx;
+    u8 dy;
     u8 tile_index;
 
     coma_reset();
     while (*map != END) {
         x = *map++;
         y = *map++;
+        rep = *map++;
         tile_index = *map++;
-        coma_set(x, y, COMA_FULL);
-        ptr = get_tile_ptr(x, y);
-        cpct_drawSprite(tile_set[tile_index], ptr, TILE_WIDTH, TILE_HEIGHT);
+        if (rep & REP_HOR) {
+            dx = 1;
+            dy = 0;
+            rep = rep & REP_MASK;
+        } else {
+            dx = 0;
+            dy = 1;
+        }
+        while (rep--) {
+            coma_set(x, y, COMA_FULL);
+            ptr = get_tile_ptr(x, y);
+            cpct_drawSprite(tile_set[tile_index], ptr, TILE_WIDTH, TILE_HEIGHT);
+            x += dx;
+            y += dy;
+        }
     }
 }
